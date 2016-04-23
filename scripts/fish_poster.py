8d9672f3d4186301e20b6372b545697aefcad015
John Lee <john@Johns-MacBook-Pro-2.local>
2016-04-23 13:34:19 -0700

    merge

--- scripts/fish_poster.py
+++ scripts/fish_poster.py
@@ -1,6 +1,6 @@
 import csv
 
-file_name = "something.csv"
+file_name = "fish.csv"
 base_url = "127.0.0.1:5000"
 insert_fish_endpoint = "/insert_fish"
 
@@ -8,6 +8,7 @@ insert_fish_endpoint = "/insert_fish"
 def insert_fish(file_name):
     with open(file_name, 'rb') as csvfile:
         csv_reader = csv.reader(csvfile)
+        json_array = []
         for row in csv_reader:
             json_obj = {
                 "name": row[0],
